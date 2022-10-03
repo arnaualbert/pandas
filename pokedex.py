ca7135119a9a4f55de512d3a6dbf6c73bffd16e8
Arnau Albert <arnaualbert@pop-os.localdomain>
2022-10-03 16:13:53 +0200

    act

--- pokedex.py
+++ pokedex.py
@@ -2,3 +2,7 @@ import numpy as np
 import pandas as pd
 pokedex: pd.DataFrame = pd.read_csv("./pokedex.csv", sep=",")
 print(pokedex)
+pokemonNames: pd.Series = pokedex['Name']
+print(pokemonNames)
+print(pokedex.loc[:,['Name','HP']])
+print(pokedex.loc[ [152,153] , : ] )
